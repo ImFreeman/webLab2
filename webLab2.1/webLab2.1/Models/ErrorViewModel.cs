fc30f12f99cc74820bc5295bd1f0c3cfa11dd5d2
filippov_filipp <imawakebrba@gmail.com>
2022-03-17 21:19:00 +0700

    Revert "comm"
    This reverts commit 9916d178aee0fe6ed9f72f698a451acbccc992d9.

--- webLab2.1/webLab2.1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace webLab2._1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
